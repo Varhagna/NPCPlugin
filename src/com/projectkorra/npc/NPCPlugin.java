72376702b40a26874cc16861c9dfbb276da01255
Vahagn Tovmasian <vahagntovmasian@gmail.com>
2019-10-08 11:34:42 -0700

    Update NPCPlugin.java
    - Add startup messages and enable configuration

--- src/com/projectkorra/npc/NPCPlugin.java
+++ src/com/projectkorra/npc/NPCPlugin.java
@@ -6,12 +6,19 @@ public class NPCPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+	System.out.println("[NPCPlugin] Enabling Config, NPCs, and Listeners");
 	
+	this.getServer().getConfiguration().setDefaults(true);
+	System.out.println("[NPCPlugin] Config Successfully Enabled");
+	    
+	System.out.println("[NPCPlugin] NPCs Successfully Enabled");
+	
+	System.out.println("[NPCPlugin] Listeners Successfully Enabled");
     }
     
     @Override
     public void onDisable() {
-	
+	System.out.println("[NPCPlugin] Saving Config, Disabling Listeners and NPCs");
     }
 
 }
